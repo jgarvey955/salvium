0d2659751616e3f23f1bd0c5ef8b23d5e91b4d7e
Jonathan Garvey <jgarvey955@gmail.com>
2026-07-03 16:07:01 -0400

    initial fix

--- src/common/debugging.h
+++ src/common/debugging.h
@@ -6,6 +6,8 @@
 #define DEBUG_BREAK() asm volatile("brk #0xf000")
 #elif defined(__arm__)
 #define DEBUG_BREAK() asm volatile(".inst 0xe7f001f0")  // Encoding for bkpt #0xf01, common for ARM32
+#elif defined(__riscv)
+#define DEBUG_BREAK() asm volatile("ebreak")
 #else
-#error "Unsupported architecture for DEBUG_BREAK"
+#define DEBUG_BREAK() __builtin_trap()
 #endif
